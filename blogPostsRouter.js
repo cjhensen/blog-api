5e639c13da0e2edf8151811c5a3dcf09516433fd
Christian Hensen <christian.j.hensen@gmail.com>
2017-08-05 12:58:24 -0400

    added POST and PUT

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -4,10 +4,10 @@
 const express = require('express');
 
 // require bodyParser
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-// use jsonParser
-// const jsonParser = bodyParser.json();
+// use jsonParser: gives access to the request bodu
+const jsonParser = bodyParser.json();
 
 // use express router for modular routes
 const router = express.Router();
@@ -34,6 +34,14 @@ BlogPosts.create(
   );
 
 
+// HTTP Status Codes:
+// 200 OK
+// 201 Created
+// 204 No content to respond with
+// 202 Accepted
+// 400 Bad request
+
+
 // GET: get all blog posts
 router.get('/', (request, response) => {
   // response.send vs response.json?
@@ -50,12 +58,56 @@ router.get('/:id', (request, response) => {
 
 
 // POST: adding a new blog post
-router.post('/', (request, response) => {
+// jsonParser gives access to request.body
+router.post('/', jsonParser, (request, response) => {
+  // make sure that these fields are in the request
+  const requiredFields = ['title', 'content', 'author'];
+  for(let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if(!(field in request.body)) {
+      const errorMessage = `Field ${field} is missing from request body.`;
+      console.log(errorMessage);
+      return response.status(400).send(errorMessage);
+    } 
+  }
+
+  // must do this outside of the for loop
+  const item = BlogPosts.create(request.body.title, request.body.content, request.body.author);
+  response.status(201).json(item);
+  // ? Why use .json and not .send
+    // response.status(200).send(item);
 });
 
 
 // PUT: updating an existing blog post by id
-router.put('/:id', (request, response) => {
+router.put('/:id', jsonParser, (request, response) => {
+  // make sure that these fields are in the request
+  const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
+  for(let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if(!(field in request.body)) {
+      const errorMessage = `Field ${field} is missing from request body.`;
+      console.log(errorMessage);
+      return response.status(400).send(errorMessage);
+    }
+  }
+
+  if(request.params.id !== request.body.id) {
+    const errorMessage = `Request id: ${request.params.id} and body id: ${request.body.id} must match`;
+    return response.status(400).send(errorMessage);
+  }
+
+  console.log(`Updating recipe: ${request.params.id}`);
+  BlogPosts.update({
+    id: request.params.id,
+    title: request.body.title,
+    content: request.body.content,
+    author: request.body.author,
+    publishDate: request.body.publishDate
+  });
+
+  response.status(204).end();
+  // ? Why no return here
 });
 
 

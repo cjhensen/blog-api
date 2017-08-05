fd78e61e3733fdc31107e6cf9aca458931ade9e6
Christian Hensen <christian.j.hensen@gmail.com>
2017-08-05 12:19:53 -0400

    get request for all posts and for a singe post by id

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -3,11 +3,18 @@
 // require express
 const express = require('express');
 
+// require bodyParser
+// const bodyParser = require('body-parser');
+
+// use jsonParser
+// const jsonParser = bodyParser.json();
+
 // use express router for modular routes
 const router = express.Router();
 
 // bring in the BlogPosts model
 const {BlogPosts} = require('./models');
+
 
 // create a few blog posts
 BlogPosts.create(
@@ -26,24 +33,36 @@ BlogPosts.create(
   'Christian Hensen'
   );
 
-// GET
-// get all blog posts on /blog-posts endpoint
+
+// GET: get all blog posts
 router.get('/', (request, response) => {
   // response.send vs response.json?
+  console.log('Getting all blog posts');
   response.json(BlogPosts.get());
 });
 
-// POST
+// GET: get a single blog post by ID
+router.get('/:id', (request, response) => {
+  const id = request.params.id;
+  console.log(`Getting Blog post for ID: ${id}`);
+  response.json(BlogPosts.get(id));
+});
+
+
+// POST: adding a new blog post
 router.post('/', (request, response) => {
 });
 
-// PUT
-router.put('/', (request, response) => {
+
+// PUT: updating an existing blog post by id
+router.put('/:id', (request, response) => {
 });
 
-// DELETE
-router.delete('/', (request, response) => {
+
+// DELETE: delete a blog post by id
+router.delete('/:id', (request, response) => {
 });
+
 
 // Export the router module
 module.exports = router;
